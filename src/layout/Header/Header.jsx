ba5bbc3edbab84577b47f48ceda2e222adcb6e35
gabriel <gabrielcastro.developer@gmail.com>
2023-07-13 16:15:38 -0300

    initial nav links

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -7,7 +7,12 @@ function Header(){
             <div className='nav-wraper'>
                 <img src={logo} alt="Starbucks logo" />
                 <nav>
-
+                    <ul>
+                        <li>MENU</li>
+                        <li>REWARDS</li>
+                        <li>NOSSO CAFÉ</li>
+                        <li>IMPACTO SOCIAL</li>
+                    </ul>
                 </nav>
             </div>
             <div className='nav-right'>
