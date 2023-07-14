a3d81c00fecbc9e5b0e95f1e74ccd4bf47b06379
gabriel <gabrielcastro.developer@gmail.com>
2023-07-14 15:51:14 -0300

    Desktop slider finished

---
+++ src/assets/banners/banners.js
@@ -0,0 +1,8 @@
+import banner01 from './banner01.png';
+import banner02 from './banner02.png';
+import banner03 from './banner03.png';
+import banner04 from './banner04.png';
+
+const banners = [banner01, banner02, banner03, banner04];
+
+export default banners;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,34 @@
 import './Home.scss';
 import Header from '../layout/Header/Header';
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import "swiper/css/navigation";
+import 'swiper/css/pagination';
+import banners from '../assets/banners/banners';
 
 function Home() {
   return (
     <>
       <Header />
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        autoplay={{delay:5000, disableOnInteraction: false,}} 
+        modules={[Navigation, Pagination, Autoplay]}
+        className="mySwiper"
+      >
+        {banners.map((banner) => 
+          <SwiperSlide>
+            <img src={banner} alt="Starbucks banner" />
+          </SwiperSlide>
+        )}
+      </Swiper>
+
     </>
   );
 }
