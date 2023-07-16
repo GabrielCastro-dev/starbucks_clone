75c3396ae5eccee50f060b9d8f677a80b8b10e7b
gabriel <gabrielcastro.developer@gmail.com>
2023-07-16 16:54:46 -0300

    social media icons

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,11 +1,20 @@
 import './Footer.scss';
+import { FaFacebookF, FaLinkedinIn, FaInstagram, FaYoutube, FaTwitter } from 'react-icons/fa6';
+import { SlSocialSpotify } from 'react-icons/sl';
 
 function Footer() {
     return (
         <footer>
             <div className='footer-row'></div>
             <hr />
-            <div className='footer-row'></div>
+            <div className='footer-row'>
+                <SlSocialSpotify className='social-media-icon' id='first-social-icon' />
+                <FaFacebookF className='social-media-icon' />
+                <FaLinkedinIn className='social-media-icon' />
+                <FaInstagram className='social-media-icon' />
+                <FaYoutube className='social-media-icon' />
+                <FaTwitter className='social-media-icon' />
+            </div>
             <div className='footer-row'></div>
         </footer>
     )
